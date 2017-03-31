b5de8c92f8900b6c4a3e9d3e44b173046286b489
Sasakin <Max_to_day@Mail.ru>
2017-04-01 01:23:33 +0300

    Update Redirect.java

--- src/ru/evm/web/servlets/Redirect.java
+++ src/ru/evm/web/servlets/Redirect.java
@@ -15,9 +15,7 @@ public class Redirect extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         count++;
 
-        System.out.println(count);
         req.setAttribute("current_count", count);
-        System.out.println("current_count " + req.getAttribute("current_count"));
         req.getRequestDispatcher("index.jsp").forward(req,resp);
     }
 }
